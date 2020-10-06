f51090cf383b50b3d4011039e2e633f08434f5b5
Volodymyr <foxandkamarus@gmail.com>
2020-10-06 16:16:30 +0300

    Added neural network algorithm

--- test_neural_network.py
+++ test_neural_network.py
@@ -1,7 +1,7 @@
 from lib.config import ConfigReader
 from lib.constants import CONFIG_FILENAME
-from lib.mt5client import Mt5Client
 from lib.currency_strategies import moving_average, get_price_percentage
+from lib.mt5client import Mt5Client
 
 config = ConfigReader(CONFIG_FILENAME).load()
 client = Mt5Client(config)
@@ -10,36 +10,87 @@ SYMBOL = 'EURUSD'
 reply = client.data(SYMBOL)
 
 total = []
-sample = []
+samples = []
+X = []
+y = []
+max_len = 0
+min_len = 99999
+sample_types_map = {
+    'sample_buy': 1,
+    'sample_sell': 2,
+    'sample_null': 3,
+    'sample_almost_sell': 4,
+    'sample_almost_buy': 5
+}
+
 for r in reply:
     if r.sample_type == 'total':
         total.append(r)
     else:
-        sample.append(r)
+        max_len = max(len(r.data), max_len)
+        min_len = min(len(r.data), min_len)
+        samples.append(r)
 
 
-#import matplotlib.pyplot as plt
-#for i, p in enumerate(reversed(list(moving_average(total[0], 50, sample[0].from_date, sample[0].to_date)))):
-#    norm_ma_50 = get_price_percentage(p, sample[0].max_price, sample[0].min_price)
-#    plt.plot(i, norm_ma_50, '.',  color='red')
+print(f'MAX: {max_len}, MIN: {min_len}')
+total = total[0]
 
-#for i, p in enumerate(reversed(list(moving_average(total[0], 20, sample[0].from_date, sample[0].to_date)))):
-#    norm_ma_20 = get_price_percentage(p, sample[0].max_price, sample[0].min_price)
-#    plt.plot(i, norm_ma_20, '.',  color='blue')
+for sample in samples:
+    rec = []
+    for ma_price in moving_average(total, 20, sample.from_date, sample.to_date):
+        norm_ma_20 = get_price_percentage(ma_price, sample.max_price, sample.min_price)
+        rec.append(norm_ma_20)
+    for ma_price in moving_average(total, 50, sample.from_date, sample.to_date):
+        norm_ma_50 = get_price_percentage(ma_price, sample.max_price, sample.min_price)
+        rec.append(norm_ma_50)
+    for currency in sample.data:
+        norm_p = get_price_percentage(currency.close, sample.max_price, sample.min_price)
+        rec.append(norm_p)
+    X.append(rec)
+    y.append(sample_types_map[sample.sample_type])
 
-#for i, p in enumerate(reversed(sample[0].data)):
-#    norm_p = get_price_percentage(p.close, sample[0].max_price, sample[0].min_price)
+# TODO. Take a look at the amount of test iris data.
+
+#print(X)
+#print(y)
+# import matplotlib.pyplot as plt
+#
+# for i, p in enumerate(total.data):
+#    norm_p = get_price_percentage(p.close, total.max_price, total.min_price)
 #    plt.plot(i, norm_p, '.',  color='green')
-
-#plt.show()
+#
+# for i, ma_price in enumerate(moving_average(total, 20, total.from_date, total.to_date)):
+#    norm_ma_20 = get_price_percentage(ma_price, total.max_price, total.min_price)
+#    plt.plot(i, norm_ma_20, '.',  color='yellow')
+#
+# for i, ma_price in enumerate(moving_average(total, 50, total.from_date, total.to_date)):
+#    norm_ma_50 = get_price_percentage(ma_price, total.max_price, total.min_price)
+#    plt.plot(i, norm_ma_50, '.',  color='red')
+#
+# plt.show()
 
 from sklearn.neural_network import MLPClassifier
-X = [[0., 0.], [1., 1.]]
-y = [0, 1]
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                    hidden_layer_sizes=(5, 2), random_state=1)
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
+
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, shuffle=True, max_iter=3000,
+                    hidden_layer_sizes=(30, 30, 30))
+
+#scores = cross_validate(clf, X, y, cv=4,
+#    scoring=('f1_weighted', 'accuracy', 'precision_weighted', 'recall_weighted'),
+#    return_train_score=True
+#)
+#for i in scores:
+#    if i.startswith('train_'):
+#        print(i, scores[i])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 clf.fit(X, y)
-print(clf.predict([[2., 2.], [-1., -2.], [-1., -1.]]))
+predictions = clf.predict(X_train)
+print(confusion_matrix(y_train,predictions))
+print(classification_report(y_train,predictions))
 
 
 # 1. Write a function to calc moving averages. DONE
@@ -49,7 +100,22 @@ print(clf.predict([[2., 2.], [-1., -2.], [-1., -1.]]))
 # 4. Display on a plot and verify with original. DONE
 
 # 5. Prepare test data for neural network and train it on these moving averages. TODO
-#     Collect all data into list and prepare for neural network.
+#     We need at least 50 examples of each sample.
+#     Use GridSearchCV to get better parameters for neural network.
+#             mlp_gs = MLPClassifier(max_iter=100)
+#             parameter_space = {
+#                 'hidden_layer_sizes': [(10,30,10),(20,)],
+#                 'activation': ['tanh', 'relu'],
+#                 'solver': ['sgd', 'adam'],
+#                 'alpha': [0.0001, 0.05],
+#                 'learning_rate': ['constant','adaptive'],
+#             }
+#             from sklearn.model_selection import GridSearchCV
+#             clf = GridSearchCV(mlp_gs, parameter_space, n_jobs=-1, cv=5)
+#             clf.fit(X, y)
+#             print('Best parameters found:\n', clf.best_params_)
+#     Calculate metrics: Accuracy , precision, recall,  f1-score,
+#     Collect all data into list and prepare for neural network. We have to collect data for sell, buy and none
 #     Train network.
 #     Run different cross validation methods.
 
