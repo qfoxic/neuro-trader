ea6b370804f130da5efa0461f9c46f0f022e0420
Volodymyr Paslavskyy <vpaslavskyy@elitex.systems>
2022-11-28 17:52:48 +0200

    Improved information output

--- dashboard/search_entry_points.py
+++ dashboard/search_entry_points.py
@@ -7,53 +7,19 @@ from dashboard.config import ConfigReader
 from dashboard.constants import CONFIG_FILENAME
 
 
-def check(currency, timeframe, precise, model_path, trade_info):
+def check(currency, timeframe, precise, model_path):
     total_samples = data(currency, os.path.join('entries', f'sdata_{currency.upper()}_{timeframe.upper()}'))
     buy_model = normalize_by_min_max(data(currency, model_path))
     sell_model = mirror_normalized_array(buy_model)
     buy_cos_sim_stream = cos_similarity1(total_samples, buy_model)
     sell_cos_sim_stream = cos_similarity1(total_samples, sell_model)
-    info = trade_info.formatted_info()
-    spread = info['spread']
-
-    pair1_impact = 'no'
-    pair2_impact = 'no'
-
-    match info[currency[0:3]]['impact']:
-        case 1:
-            pair1_impact = 'positive'
-        case 2:
-            pair1_impact = 'negative'
-
-    match info[currency[-3:]]['impact']:
-        case 1:
-            pair2_impact = 'positive'
-        case 2:
-            pair2_impact = 'negative'
-
-    pair1_news_in = int((info[currency[0:3].upper()]["time"]) / 60)
-    pair2_news_in = int((info[currency[-3:].upper()]["time"]) / 60)
-    event1 = info[currency[0:3].upper()]["event"]
-    event2 = info[currency[-3:].upper()]["event"]
-
-    print(f'DEBUG: {info}')
-    print(f'DEBUG: spread {spread}, {currency[0:3].upper()} news {event1} in {pair1_news_in if pair1_news_in <= 30 else "N/A"} minutes, impact {pair1_impact}')
-    print(f'DEBUG: spread {spread}, {currency[-3:].upper()} news {event2} in {pair2_news_in if pair2_news_in <= 30 else "N/A"} minutes, impact {pair2_impact}')
 
     for _, sim in buy_cos_sim_stream:
         if sim > precise:
-            print(
-                f'{currency} {timeframe} BUY -> {sim}, spread: {spread}\n' +
-                f'   {currency[0:3].upper()} news {event1} in {pair1_news_in if pair1_news_in <= 30 else "N/A"} minutes, impact {pair1_impact}\n'
-                f'   {currency[-3:].upper()} news {event2} in {pair2_news_in if pair2_news_in <= 30 else "N/A"} minutes, impact {pair2_impact}'
-            )
+            print(f'{currency} {timeframe} BUY -> {sim}')
     for _, sim in sell_cos_sim_stream:
         if sim > precise:
-            print(
-                f'{currency} {timeframe} SELL -> {sim}, spread: {spread}\n' +
-                f'   {currency[0:3].upper()} news {event1} in {pair1_news_in if pair1_news_in <= 30 else "N/A"} minutes, impact {pair1_impact}\n'
-                f'   {currency[-3:].upper()} news {event2} in {pair2_news_in if pair2_news_in <= 30 else "N/A"} minutes, impact {pair2_impact}'
-            )
+            print(f'{currency} {timeframe} SELL -> {sim}')
 
 
 if __name__ == '__main__':
@@ -64,13 +30,41 @@ if __name__ == '__main__':
 
     config = ConfigReader(CONFIG_FILENAME).load()
     client = Mt5Client(config)
-    print('==== Checking for entry points ===========')
     for currency in config['common']['currencies']:
         if not currency:
             break
+        print(f'======== Verifying {currency}')
+        trade_info = client.get_info(currency)
+        if trade_info:
+            info = trade_info.formatted_info()
+            spread = info['spread']
+
+            pair1_impact = 'no'
+            pair2_impact = 'no'
+
+            match info[currency[0:3]]['impact']:
+                case 1:
+                    pair1_impact = 'positive'
+                case 2:
+                    pair1_impact = 'negative'
+
+            match info[currency[-3:]]['impact']:
+                case 1:
+                    pair2_impact = 'positive'
+                case 2:
+                    pair2_impact = 'negative'
+
+            pair1_news_in = int((info[currency[0:3].upper()]["time"]) / 60)
+            pair2_news_in = int((info[currency[-3:].upper()]["time"]) / 60)
+            event1 = info[currency[0:3].upper()]["event"]
+            event2 = info[currency[-3:].upper()]["event"]
+            print(f'Spread {spread}')
+            print(f'{currency[0:3].upper()} news {event1} in {pair1_news_in if pair1_news_in <= 30 else "N/A"} minutes, impact {pair1_impact}')
+            print(f'{currency[-3:].upper()} news {event2} in {pair2_news_in if pair2_news_in <= 30 else "N/A"} minutes, impact {pair2_impact}')
+
+
         for timeframe in config['common']['timeframes']:
             if not timeframe:
                 break
             client.srates_all_stored(currency, timeframe, os.path.join('entries', f'sdata_{currency.upper()}_{timeframe.upper()}'))
-            info = client.get_info(currency)
-            check(currency, timeframe, options.precise, options.model, info)
+            check(currency, timeframe, options.precise, options.model)
